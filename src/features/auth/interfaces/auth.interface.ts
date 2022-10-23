633535cc39ab46afd11913b214b49a2459a86126
Argha Chandra Das <arghadas@Arghas-MacBook-Air.local>
2022-10-23 11:31:55 +0530

    schema and interface creation for auth

---
+++ src/features/auth/interfaces/auth.interface.ts
@@ -0,0 +1,47 @@
+import { Document } from 'mongoose';
+import { ObjectId } from 'mongodb';
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: AuthPayload; // creating/adding own property in pre-existing interface.
+      // it is optional because if user is not logged in
+    }
+  }
+}
+
+export interface AuthPayload {
+  userId: string;
+  uId: string;
+  email: string;
+  username: string;
+  avatarColor: string;
+  iat?: number;
+}
+
+export interface IAuthDocument extends Document {
+  _id: string | ObjectId;
+  uId: string;
+  username: string;
+  email: string;
+  password?: string; // optional because when we send data to user, we shouldn't send password.
+  avatarColor: string;
+  createdAt: Date;
+  passwordResetToken?: string;
+  passwordResetExpires?: number | string;
+  comparePassword(password: string): Promise<boolean>;
+  hashPassword(password: string): Promise<string>;
+}
+
+export interface ISignUpData {
+  _id: ObjectId;
+  uId: string;
+  email: string;
+  username: string;
+  password: string;
+  avatarColor: string;
+}
+
+export interface IAuthJob {
+  value?: string | IAuthDocument;
+}
